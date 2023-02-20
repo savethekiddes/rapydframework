0193f57637c8c07dc1cc3f617aecf3788d27ffed
savethekiddes <88157500+savethekiddes@users.noreply.github.com>
2023-02-20 21:09:28 +0100

    Add files via upload

--- rapydframework.py
+++ rapydframework.py
@@ -300,23 +300,24 @@ app = Flask(__name__, template_folder=buildfolder )
 def get_root():
     return render_template("app.html")
 
-@app.route('/<folder>')
-def get_template(folder):
-    template_path = os.path.join(folder, 'index.html')
-    file_path = buildfolder + folder
-    if os.path.exists(buildfolder + template_path):
-        return render_template(template_path.replace("\\", "/"))
-    elif os.path.exists(file_path):
-        return send_from_directory(buildfolder, folder)
-    else:
-        abort(404)
+@app.route('/<path:path>')
+def serve_file(path=''):
+    file_path = os.path.join('../build/', path)
 
+    if os.path.isfile(file_path):
+        return send_from_directory('../build/', path)
+    elif os.path.isdir(file_path):
+        index_path = os.path.join(file_path, 'index.html')
+        if os.path.isfile(index_path):
+            return send_from_directory(file_path, 'index.html')
+    abort(404)
 
 # FROM THIS POINT YOU CAN'T SETUP ROUTES ANYMORE AND THE FLASK SERVER WILL NOT RESTART
 if __name__ == '__main__':
     app.run(debug=True, port="9999")""")
 	print("Starting a Flask development server...")
-	subprocess.run(["python", "dev/devserver.py"])
+	os.chdir('/dev')
+	subprocess.run(["python", "devserver.py"])
 	sys.exit()
 
 print("No argument submitted, please see -h or --help")
