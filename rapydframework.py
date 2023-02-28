c3ca9951ed29a12c85cbf2ad1f30890fb5900d5d
savethekiddes <88157500+savethekiddes@users.noreply.github.com>
2023-02-28 15:04:48 +0100

    Removes (temporarely) minification

--- rapydframework.py
+++ rapydframework.py
@@ -257,15 +257,7 @@ if args.compile:
 						env=os.environ)
 					print(result.stdout.decode())
 					print(result.stderr.decode(), file=sys.stderr)
-    
-    # Minifies the build
-	if not args.debug:
-		for root, dirs, files in os.walk("build/"):
-			for file in files:
-				if file.endswith(".css" or ".html" or ".js"):
-					minpath = os.path.join(root, file).replace("\\", "/")
-					print(os.path.abspath(minpath))
-					subprocess.run(["minify.cmd", os.path.abspath(minpath), ">", os.path.abspath(minpath)], env=os.environ)
+
     
     # Deletes the temporary files
 	shutil.rmtree("temp/")
