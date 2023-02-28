0cbbed0fce06bf003c5bacd5ac8dc1f6b495cb48
savethekiddes <88157500+savethekiddes@users.noreply.github.com>
2023-02-28 14:53:46 +0100

    Add files via upload

--- rapydframework.py
+++ rapydframework.py
@@ -264,7 +264,7 @@ if args.compile:
 			for file in files:
 				if file.endswith(".css" or ".html" or ".js"):
 					minpath = os.path.join(root, file).replace("\\", "/")
-					subprocess.run(["minify.cmd", minpath, ">", minpath], env=os.environ)
+					subprocess.run(["minify.cmd", os.path.abspath(minpath), ">", os.path.abspath(minpath)], env=os.environ)
     
     # Deletes the temporary files
 	shutil.rmtree("temp/")
